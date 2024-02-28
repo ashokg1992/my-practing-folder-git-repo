f8c98070a0cdffb4622daa4131ef135ff67aaa91
ashok <ashokreddyothers@gmail.com>
2024-02-28 16:35:41 +0530

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # my-practing-folder-git-repo
+# git add . ; git commit -m "add" ; git push origin main
--- shell/exit.sh
+++ shell/exit.sh
@@ -1,1 +1,7 @@
-ag
+#!/bin/bash
+wget https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war >> /dev/null
+if [ $? -eq 0 ]; then
+echo "file downloaded" 
+else
+echo "provide proper link to download"
+fi
